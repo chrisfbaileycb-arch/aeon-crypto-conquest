e4c10f401f58aae2058dd68e6c9cdbdad40e3cc0
chrisfbaileycb-arch <chrisfbailey.cb@gmail.com>
2026-08-16 21:24:01 -0600

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig={appId:'com.signalf.aeonconquest',appName:'Aeon Crypto Conquest',webDir:'dist',server:{androidScheme:'https'},android:{allowMixedContent:false}};
+export default config;
